01b4f64ca37b8389f6b5500e9c34762327d8bc2f
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-10-31 16:46:52 -0300

    Alteraçao da origin do server

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://projeto-socket.vercel.app/",
         methods: ["GET", "POST"]
     }
 })
